021c55e727e62b7af5d1c1bb426ca6d1e6026fe7
Ash <ashe.null@protonmail.ch>
2021-07-29 18:22:00 +0000

    fix missing YamlDotNet artifact

--- pack.sh
+++ pack.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-LATEST_UNITY_SUBSET_35=https://ci.appveyor.com/api/buildjobs/yaovugrbkveb7dry/artifacts/YamlDotNet%2Fbin%2FRelease%2FRelease-UnitySubset-v35.zip
-
 final=$1
 if [[ -z "$final" ]]; then
 	echo Final destination required
@@ -10,18 +8,13 @@ fi
 tmp=$(mktemp -d)
 patchers=$tmp/patchers
 dest=$tmp/dest.zip
-yaml_zip=$(mktemp)
 
 mkdir -p "$patchers"
-
-curl -o "$yaml_zip" -L "$LATEST_UNITY_SUBSET_35"
-unzip -p "$yaml_zip" YamlDotNet.dll > "$patchers/YamlDotNet.dll"
-
-rm -rf "$yaml_zip"
 
 cp README.md Mason.Patcher/manifest.json "$tmp/"
 cp media/icon/256.png "$tmp/icon.png"
 cp Mason.Patcher/bin/Release/net35/{Mason.Core,Mason.Patcher,Newtonsoft.Json}.dll "$patchers/"
+cp libs/YamlDotNet_UnitySubset35.dll "$patchers/YamlDotNet.dll"
 
 pushd "$tmp"
 zip -9r "$dest" .
